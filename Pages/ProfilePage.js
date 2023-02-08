a74ccd470454a57053bb30ccb45912d70a688e51
D <technicallyanemail22@gmail.com>
2023-02-08 17:37:57 -0500

    starting on the ProfilePage

--- Pages/ProfilePage.js
+++ Pages/ProfilePage.js
@@ -1,19 +1,44 @@
 import React from 'react';
-import {Text, View} from 'react-native';
-
-
+import { Text, View } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
 
 const ProfilePage = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <Text>Hello, World!!!!!!</Text>
+    <View>
+      <View style={styles.headerContainer}>
+        <Text style={styles.headerText}>Profile</Text>
+      </View>
+      <ScrollView>
+        <View style={{ flexDirection: 'row', position: 'center' }}>
+          {/*Insert Picture here, copy import camera api from Extra.js*/}
+          <Icon name="circle" size={75} color="white" />
+        </View>
+      </ScrollView>
     </View>
   );
+}
+
+const styles = {
+
+  //Header
+  headerContainer: {
+    backgroundColor: '#f2f2f2',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 15,
+    paddingBottom: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 3 },
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative',
+    flexDirection: 'row',
+  },
+  headerText: {
+    fontSize: 20,
+    flex: 0.95,
+    textAlign: 'center',
+  }
 };
 
 
